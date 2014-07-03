d84f832065e51eeb6af862dd68f3dd534a683933
Amanda McDermott <amandapmcdermott@gmail.com>
2014-07-03 11:37:46 -0400

    Added model and migration for songs, belonging to artists

---
+++ db/migrate/20140703151601_create_songs.rb
@@ -0,0 +1,13 @@
+class CreateSongs < ActiveRecord::Migration
+  def change
+    create_table :songs do |t|
+      t.text :title
+      t.belongs_to :artist, index: true
+      t.integer :year
+      t.text :key
+      t.integer :range_low
+      t.integer :range_high
+      t.timestamps
+    end
+  end
+end
