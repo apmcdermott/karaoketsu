1ebfac4675f7cb5166ceae3b07ee855ef54c8c32
Amanda McDermott <amandapmcdermott@gmail.com>
2014-07-03 12:33:07 -0400

    Midi to note name converstion method now returns the letter as well as the proper octave

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,9 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def midi_to_named(midi)
-    note_letters = %w(A B C D E F G)
+    note_letters = %w(A A# B C C# D D# E F F# G G#)
     octave = midi / 12 - 1
+    index = midi % 12 + 3
+    note_letters[index] + octave.to_s
   end
 end
