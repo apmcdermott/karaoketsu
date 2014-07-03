ea2d814ba281bdef7a45b9ac0af8e0ac698ca8af
Amanda McDermott <amandapmcdermott@gmail.com>
2014-07-03 11:38:11 -0400

    Ran migrations for Songs and Artists

---
+++ db/schema.rb
@@ -0,0 +1,43 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20140703151601) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "artists", force: true do |t|
+    t.text     "name"
+    t.text     "alt_name"
+    t.text     "gender"
+    t.integer  "range_low"
+    t.integer  "range_high"
+    t.text     "voice_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "songs", force: true do |t|
+    t.text     "title"
+    t.integer  "artist_id"
+    t.integer  "year"
+    t.text     "key"
+    t.integer  "range_low"
+    t.integer  "range_high"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "songs", ["artist_id"], name: "index_songs_on_artist_id", using: :btree
+
+end
