fe29835da500361e8bd1c4b20b8ad7f9f8f96406
Amanda McDermott <amandapmcdermott@gmail.com>
2014-07-07 16:33:26 -0400

    removed bonus comments in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,5 @@
 Rails.application.routes.draw do
   root 'songs#index'
-
   resources :artists
   resources :songs
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
 end
