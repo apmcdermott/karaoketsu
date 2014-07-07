de83cd68889d58277e860e3ef9ea0c4289d632c0
Amanda McDermott <amandapmcdermott@gmail.com>
2014-07-07 16:31:16 -0400

    Added index and show to artists controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,19 @@
 class ArtistsController < ApplicationController
+  def index
+    @artists = Artist.all
+
+    # respond_to do |format|
+    #   format.html
+    #   format.json { render json: @artists }
+    # end
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+
+    # respond_to do |format|
+    #   format.html
+    #   format.json { render json: @artist }
+    # end
+  end
 end
