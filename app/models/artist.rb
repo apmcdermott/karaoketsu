aa775047d33279dc815b14be3d77bfbf0bb43078
Amanda McDermott <amandapmcdermott@gmail.com>
2014-07-07 16:32:09 -0400

    Added midi converter to artists model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,10 @@
 class Artist < ActiveRecord::Base
   has_many :songs
+
+  def midi_to_named(midi)
+    note_letters = %w(C C# D D# E F F# G G# A A# B)
+    octave = midi / 12 - 1
+    index = midi % 12
+    note_letters[index]+octave.to_s
+  end
 end
