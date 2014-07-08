4a3147bafa125f035ac702a4abcede7724e2eacf
Amanda McDermott <amandapmcdermott@gmail.com>
2014-07-08 15:52:47 -0400

    Added sortable helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def sortable(column, title, direction = "asc")
+    title ||= column.titleize
+    css_class = (column == sort_column) ? "current #{sort_direction}" : nil
+
+    if column == sort_column && sort_direction == "asc"
+      direction = "desc"
+    elsif column == sort_column && sort_direction == "desc"
+      direction = "asc"
+    else
+      direction
+    end
+
+    link_to title, {sort: column, direction: direction}, {:class => css_class}
+  end
 end
