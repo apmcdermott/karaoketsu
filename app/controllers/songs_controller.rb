042c478c182b0409ab51fbc9ec82da818767937b
Amanda McDermott <amandapmcdermott@gmail.com>
2014-07-08 15:06:30 -0400

    Added songs sorting functionality

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,14 @@
 class SongsController < ApplicationController
 
   def index
-    @songs = Song.all
+    if params[:tag]
+      @songs = Song.joins(:artist).tagged_with(params[:tag]).order(params[:sort]).preload(:artist)
+      tag_name = params[:tag]
+      @page_title = "Tagged with '#{tag_name}'"
+    else
+      @songs = Song.joins(:artist).all.order(params[:sort])
+      @page_title = "All Songs"
+    end
   end
 
   def show
