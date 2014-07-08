c9b5b8f15a8457ea2ef43db40accff096fdf3069
Amanda McDermott <amandapmcdermott@gmail.com>
2014-07-08 16:36:03 -0400

    Added sort functionality to artists index stuff

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,10 +1,22 @@
 class ArtistsController < ApplicationController
   def index
-    @artists = Artist.all.order(params[:sort])
+    @artists = Artist.all.order(sort_column + ' ' + sort_direction)
   end
 
   def show
     @artist = Artist.find(params[:id])
     @songs = @artist.songs
   end
+
+  private
+
+
+  def sort_column
+    %w(name gender voice_type range_high range_low).include?(params[:sort]) ? params[:sort] : "name"
+  end
+
+  def sort_direction
+    %w(asc desc).include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
 end
