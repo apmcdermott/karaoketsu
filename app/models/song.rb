d1f9307157dc18db008068ccd7e6b18de3602a2e
Amanda McDermott <amandapmcdermott@gmail.com>
2014-07-08 11:27:46 -0400

    Added acts_as_taggable on song model

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,6 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+  acts_as_taggable # Alias for acts_as_taggable_on :tags
 
   def midi_to_named(midi)
     note_letters = %w(C C# D D# E F F# G G# A A# B)
