871db12e8eb23a67d44912926348e1445a39e3de
Amanda McDermott <amandapmcdermott@gmail.com>
2014-07-08 15:05:28 -0400

    Added artist_name delegate to song model

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,7 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
   acts_as_taggable # Alias for acts_as_taggable_on :tags
+  delegate :name, to: :artist, prefix: true, allow_nil: true
 
   def midi_to_named(midi)
     note_letters = %w(C C# D D# E F F# G G# A A# B)
