a3250b75a3a96dbc3c1213d9b67e147ff5c6ee9e
Amanda McDermott <amandapmcdermott@gmail.com>
2014-07-10 13:17:37 -0400

    Commented out midi_to_named method in models

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,12 +3,12 @@ class Song < ActiveRecord::Base
   acts_as_taggable
   delegate :name, to: :artist, prefix: true, allow_nil: true
 
-  def midi_to_named(midi)
-    note_letters = ["C", "C\u266F", "D", "E\u266D", "E", "F", "F\u266F", "G", "A\u266D", "A", "B\u266D", "B"]
-    octave = midi / 12 - 1
-    index = midi % 12
-    note_letters[index]+octave.to_s
-  end
+  # def midi_to_named(midi)
+  #   note_letters = ["C", "C\u266F", "D", "E\u266D", "E", "F", "F\u266F", "G", "A\u266D", "A", "B\u266D", "B"]
+  #   octave = midi / 12 - 1
+  #   index = midi % 12
+  #   note_letters[index]+octave.to_s
+  # end
 
   def key_name(key_number, mode_number)
     key_letters = ["C", "C\u266F", "D", "E\u266D", "E", "F", "F\u266F", "G", "A\u266D", "A", "B\u266D", "B"]
