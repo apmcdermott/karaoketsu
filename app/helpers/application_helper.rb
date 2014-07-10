b29ad04797b4d776f18c2f737befbb8129ce8ff6
Amanda McDermott <amandapmcdermott@gmail.com>
2014-07-10 13:18:05 -0400

    Moved midi_to_named method to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+
+  def midi_to_named(midi)
+    note_letters = ["C", "C\u266F", "D", "E\u266D", "E", "F", "F\u266F", "G", "A\u266D", "A", "B\u266D", "B"]
+    octave = midi / 12 - 1
+    index = midi % 12
+    note_letters[index]+octave.to_s
+  end
+
   def sortable(column, title, direction = "asc")
     title ||= column.titleize
     css_class = (column == sort_column) ? "sorter current #{sort_direction}" : "sorter disabled"
