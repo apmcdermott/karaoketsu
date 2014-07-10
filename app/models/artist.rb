1aa04d214204c3b67acc94acef5750a64aa048b2
Amanda McDermott <amandapmcdermott@gmail.com>
2014-07-10 13:24:36 -0400

    Removed old midi_to_named definitions from artist and song models

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,13 +3,6 @@ class Artist < ActiveRecord::Base
   acts_as_taggable
   accepts_nested_attributes_for :songs, allow_destroy: true
 
-  # def midi_to_named(midi)
-  #   note_letters = ["C", "C\u266F", "D", "E\u266D", "E", "F", "F\u266F", "G", "A\u266D", "A", "B\u266D", "B"]
-  #   octave = midi / 12 - 1
-  #   index = midi % 12
-  #   note_letters[index]+octave.to_s
-  # end
-
   rails_admin do
     field :name
     field :alt_name
