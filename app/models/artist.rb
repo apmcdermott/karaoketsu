2cdb6e9be27e9140f0104ee3151fabc50ec84cc6
Amanda McDermott <amandapmcdermott@gmail.com>
2014-07-10 23:23:16 -0400

    Added has_many relation btw artist and user models

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,6 @@
 class Artist < ActiveRecord::Base
   has_many :songs
+  has_many :users
   acts_as_taggable
   accepts_nested_attributes_for :songs, allow_destroy: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   acts_as_taggable
+  belongs_to :artist
 end
