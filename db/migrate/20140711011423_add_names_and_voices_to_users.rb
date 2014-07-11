401e9d0947688e849a6a914303e24d0200900321
Amanda McDermott <amandapmcdermott@gmail.com>
2014-07-10 22:07:20 -0400

    Gave user voice stuff some default values so not null

--- db/migrate/20140711011423_add_names_and_voices_to_users.rb
+++ db/migrate/20140711011423_add_names_and_voices_to_users.rb
@@ -2,9 +2,7 @@ class AddNamesAndVoicesToUsers < ActiveRecord::Migration
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
-    add_column :users, :sounds_like, :integer
-    add_column :users, :voice_tags, :string
-    add_column :users, :range_low, :integer
-    add_column :users, :range_high, :integer
+    add_column :users, :range_low, :integer, default: 0
+    add_column :users, :range_high, :integer, default: 127
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140709152213) do
+ActiveRecord::Schema.define(version: 20140711011423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,10 @@ ActiveRecord::Schema.define(version: 20140709152213) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",                  default: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "range_low",              default: 0
+    t.integer  "range_high",             default: 127
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
