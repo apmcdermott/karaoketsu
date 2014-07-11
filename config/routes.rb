dd83f4f8d47352d832c821151899dec2db48135a
Amanda McDermott <amandapmcdermott@gmail.com>
2014-07-11 01:03:00 -0400

    Routed the root to home#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
+  # Allows for additional fields in Devise
   devise_for :users, :controllers => { registrations: 'registrations' }
+
+  # Authenticated users to go their dashboard
+  authenticated do
+    root :to => 'home#dashboard', as: :authenticated
+  end
+
+  # Non-authenticated users to go static homepage
+  root :to => 'home#index'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root 'artists#index'
+
   resources :artists
   resources :songs
+
+  # Routes tag searches to songs index action
   get 'tags/:tag', to: 'songs#index', as: :tag
 end
