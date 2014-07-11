9bef2b59c749e472ce8fea5151c7c1826a9e4437
Amanda McDermott <amandapmcdermott@gmail.com>
2014-07-10 20:59:22 -0400

    made it so only admins can access rails admin dash

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,10 +1,17 @@
 RailsAdmin.config do |config|
 
   # == Devise ==
-  config.authenticate_with do
-    warden.authenticate! scope: :user
+  # config.authenticate_with do
+  #   warden.authenticate! scope: :user
+  # end
+  # config.current_user_method(&:current_user)
+
+  config.authorize_with do |controller|
+    unless current_user.try(:admin?)
+      flash[:error] = "You are not an admin"
+      redirect_to main_app.root_path([])
+    end
   end
-  config.current_user_method(&:current_user)
 
   config.actions do
     dashboard                     # mandatory
