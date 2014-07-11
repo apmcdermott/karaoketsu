654e12714cba4d6956f2a815e33b470463a55c50
Amanda McDermott <amandapmcdermott@gmail.com>
2014-07-11 04:06:01 -0400

    Added bootstrap colors to flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,4 +24,19 @@ module ApplicationHelper
 
     link_to title, {sort: column, direction: direction}, {:class => css_class}
   end
+
+  def bootstrap_class_for(flash_type)
+    case flash_type
+      when "success"
+        "alert-success"   # Green
+      when "error"
+        "alert-danger"    # Red
+      when "alert"
+        "alert-warning"   # Yellow
+      when "notice"
+        "alert-info"      # Blue
+      else
+        flash_type.to_s
+    end
+  end
 end
