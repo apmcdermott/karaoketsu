30f5439cc6ca264ea60df46ac4c1bf187550f544
Amanda McDermott <amandapmcdermott@gmail.com>
2014-07-11 15:09:54 -0400

    Rails serves static assets now

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.serve_static_assets = true
+
 module Karaketsu
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
