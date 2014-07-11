3d3f2bce686a7d36a1f6f1df2efe0e9133a36ab1
Amanda McDermott <amandapmcdermott@gmail.com>
2014-07-11 01:01:22 -0400

    Added set_artists method to registration controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,17 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_action :set_artists
+
+  def set_artists
+    @artists = Artist.all
+  end
 
   private
 
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :sounds_like, :range_low, :range_high)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :artist_id, :range_low, :range_high)
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :sounds_like, :range_low, :range_high)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :artist_id, :range_low, :range_high)
   end
 end
