b6ebab2bbc2964a1357506e1f0d0aaf5ac9c4edc
Amanda McDermott <amandapmcdermott@gmail.com>
2014-07-10 20:58:43 -0400

    Edited seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,50 +4,74 @@ Song.delete_all
 User.delete_all
 
 puts "Creating admin"
-mandy = User.create(email: "mandy@example.com", password: "amoreappropriatepassword", admin: true)
+mandy = User.create(email: "mandy@example.com", password: "password", admin: true)
+
+singers = []
 
 puts "Creating dude singers"
-mjackson = Artist.create(name: "Michael Jackson", gender: "male", range_low: 61, range_high: 85, voice_type: "Tenor")
-jlegend = Artist.create(name: "John Legend", gender: "male", range_low: 60, range_high: 82, voice_type: "Baritone")
-jtimberlake = Artist.create(name:"Justin Timberlake", gender: "male", range_low: 42, range_high: 83, voice_type: "Tenor")
-esheeran = Artist.create(name:"Ed Sheeran", gender: "male", range_low: 57, range_high: 81, voice_type: "Tenor")
+singers << mjackson = Artist.create(name: "Michael Jackson", gender: "male", range_low: 61, range_high: 85, voice_type: "tenor")
+singers << jlegend = Artist.create(name: "John Legend", gender: "male", range_low: 60, range_high: 82, voice_type: "baritone")
+singers << jtimberlake = Artist.create(name:"Justin Timberlake", gender: "male", range_low: 42, range_high: 83, voice_type: "tenor")
+singers << esheeran = Artist.create(name:"Ed Sheeran", gender: "male", range_low: 57, range_high: 81, voice_type: "tenor")
 
 puts "Creating lady singers"
-nimbruglia = Artist.create(name: "Natalie Imbruglia", gender: "female", range_low: 53, range_high: 70, voice_type: "Soprano")
-cunderwood = Artist.create(name: "Carrie Underwood", gender: "female", range_low: 50, range_high: 80, voice_type: "Mezzo-Soprano")
-cperri = Artist.create(name: "Christina Perri", gender: "female", range_low: 53, range_high: 72, voice_type: "Contralto")
-imenzel = Artist.create(name: "Idina Menzel", gender: "female", range_low: 53, range_high: 82, voice_type: "Mezzo-Soprano")
-adele = Artist.create(name: "Adele", gender: "female", range_low: 48, range_high: 76, voice_type: "Contralto")
-awinehouse = Artist.create(name: "Amy Winehouse", gender: "female", range_low: 50, range_high: 87, voice_type: "Contralto")
-hwilliams = Artist.create(name: "Hayley Williams", alt_name: "Paramore", gender: "female", range_low: 46, range_high: 78, voice_type: "Mezzo-Soprano")
+singers << nimbruglia = Artist.create(name: "Natalie Imbruglia", gender: "female", range_low: 53, range_high: 70, voice_type: "soprano")
+singers << cunderwood = Artist.create(name: "Carrie Underwood", gender: "female", range_low: 50, range_high: 80, voice_type: "mezzo-soprano")
+singers << cperri = Artist.create(name: "Christina Perri", gender: "female", range_low: 53, range_high: 72, voice_type: "contralto")
+singers << imenzel = Artist.create(name: "Idina Menzel", gender: "female", range_low: 53, range_high: 82, voice_type: "mezzo-soprano")
+singers << adele = Artist.create(name: "Adele", gender: "female", range_low: 48, range_high: 76, voice_type: "contralto")
+singers << awinehouse = Artist.create(name: "Amy Winehouse", gender: "female", range_low: 50, range_high: 87, voice_type: "contralto")
+singers << hwilliams = Artist.create(name: "Hayley Williams", alt_name: "Paramore", gender: "female", range_low: 46, range_high: 78, voice_type: "Mezzo-soprano")
 
+songs = []
 puts "Creating songs"
-billiejean = Song.create(title: "Billie Jean", artist: mjackson, range_low: 61, range_high: 85, key: "F\u266F", mode: "minor")
-someonelikeyou = Song.create(title: "Someone Like You", artist: adele, range_low: 52, range_high: 76, key: "A", mode: "major")
-torn = Song.create(title: "Torn", artist: nimbruglia, range_low: 53, range_high: 70, key: "Gb", mode: "major")
-beforehecheats = Song.create(title: "Before He Cheats", artist: cunderwood, range_low: 54, range_high: 73, key: "F#", mode: "minor")
-cowboycasanova = Song.create(title: "Cowboy Casanova", artist: cunderwood, range_low: 57, range_high: 74, key: "D", mode: "minor")
-athousandyears = Song.create(title: "A Thousand Years", artist: cperri, range_low: 53, range_high: 72, key: "Bb", mode: "major")
-allofme = Song.create(title: "All of Me", artist: jlegend, range_low: 60, range_high: 82, key: "Ab", mode: "major")
-letitgo = Song.create(title: "Let It Go", artist: imenzel, range_low: 53, range_high: 75, key: "Ab", mode: "major")
-legohouse = Song.create(title: "Lego House", artist: esheeran, range_low: 57, range_high: 81, key: "A", mode: "major")
-crymeariver = Song.create(title: "Cry Me a River", artist: jtimberlake, range_low: 60, range_high: 83, key: "G#", mode: "minor")
-stillintoyou = Song.create(title: "Still Into You", artist: hwilliams, range_low: 0, range_high: 127)
+songs << billiejean = Song.create(title: "Billie Jean", artist: mjackson, range_low: 61, range_high: 85, key: 6, mode: 0)
+songs << someonelikeyou = Song.create(title: "Someone Like You", artist: adele, range_low: 52, range_high: 76, key: 9, mode: 1)
+songs << torn = Song.create(title: "Torn", artist: nimbruglia, range_low: 53, range_high: 70, key: 6, mode: 1)
+songs << beforehecheats = Song.create(title: "Before He Cheats", artist: cunderwood, range_low: 54, range_high: 73, key: 6, mode: 0)
+songs << cowboycasanova = Song.create(title: "Cowboy Casanova", artist: cunderwood, range_low: 57, range_high: 74, key: 2, mode: 0)
+songs << athousandyears = Song.create(title: "A Thousand Years", artist: cperri, range_low: 53, range_high: 72, key: 10, mode: 1)
+songs << allofme = Song.create(title: "All of Me", artist: jlegend, range_low: 60, range_high: 82, key: 8, mode: 1)
+songs << letitgo = Song.create(title: "Let It Go", artist: imenzel, range_low: 53, range_high: 75, key: 8, mode: 1)
+songs << legohouse = Song.create(title: "Lego House", artist: esheeran, range_low: 57, range_high: 81, key: 9, mode: 1)
+songs << crymeariver = Song.create(title: "Cry Me a River", artist: jtimberlake, range_low: 60, range_high: 83, key: 8, mode: 0)
+songs << stillintoyou = Song.create(title: "Still Into You", artist: hwilliams, range_low: 60, range_high: 74, key: 5, mode: 0)
+songs << onlyexception = Song.create(title: "The Only Exception", artist: hwilliams, range_low: 54, range_high: 75, key: 11, mode: 1)
+songs << aintitfun = Song.create(title: "Ain't It Fun", artist: hwilliams, range_low: 59, range_high: 73, key: 4, mode: 1)
 
-puts "Adding tags"
+puts "Adding voice tags"
+mjackson.tag_list.add("falsetto", "breathy", "nasal")
+jlegend.tag_list.add("belting", "smoky")
+jtimberlake.tag_list.add("falsetto", "complex vocal runs", "nasal")
+nimbruglia.tag_list.add("bright", "sweet")
+cunderwood.tag_list.add("belting", "bright")
+cperri.tag_list.add("breathy", "sweet")
+esheeran.tag_list.add("breathy", "complex vocal runs")
+awinehouse.tag_list.add("dark", "smoky")
+hwilliams.tag_list.add("bright", "youthful")
+adele.tag_list.add("warm", "soulful")
+
+puts "Saving artist tags"
+singers.each do |singer|
+  singer.save
+end
+
+puts "Adding song tags"
+billiejean.tag_list.add("falsetto", "funky", "pop")
 someonelikeyou.tag_list.add("belting", "warm", "soulful")
-torn.tag_list.add("bright")
-beforehecheats.tag_list.add("belting", "sassy")
-cowboycasanova.tag_list.add("belting")
-athousandyears.tag_list.add("breathy", "sweet")
-allofme.tag_list.add("belting", "falsetto")
-letitgo.tag_list.add("belting", "bright")
+torn.tag_list.add("bright", "love", "pop")
+beforehecheats.tag_list.add("belting", "bright", "sassy", "country")
+cowboycasanova.tag_list.add("belting", "bright", "sassy", "country")
+athousandyears.tag_list.add("breathy", "sweet", "simple", "love")
+allofme.tag_list.add("belting", "smoky", "falsetto", "love", "ballad")
+letitgo.tag_list.add("belting", "bright", "anthemic", "soundtrack")
+legohouse.tag_list.add("love", "breathy", "acoustic", "complex vocal runs")
+crymeariver.tag_list.add("falsetto", "complex vocal runs", "nasal", "funky", "pop")
+stillintoyou.tag_list.add("rock", "love", "bright", "youthful", "anthemic")
+onlyexception.tag_list.add("love", "acoustic", "rock", "bright", "youthful")
+aintitfun.tag_list.add("rock", "bright", "youthful", "layered harmonies")
 
-puts "Saving tags"
-someonelikeyou.save
-torn.save
-beforehecheats.save
-cowboycasanova.save
-athousandyears.save
-allofme.save
-letitgo.save
+puts "Saving song tags"
+songs.each do |song|
+  song.save
+end
