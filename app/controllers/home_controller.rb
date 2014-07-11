db7c55b804e867756f58e2bc82e801bbb323be48
Amanda McDermott <amandapmcdermott@gmail.com>
2014-07-11 02:36:04 -0400

    Added instance variables that would search from tagged terms

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,11 @@
 class HomeController < ApplicationController
 
   def index
-    @tags = Song.tag_counts_on(:tags)
   end
 
   def dashboard
+    @songs_in_range = Song.where("range_low >= ? AND range_high <= ?", current_user.range_low, current_user.range_high)
+    @songs_in_style = Song.tagged_with(current_user.artist.tag_list)
   end
 
 end
